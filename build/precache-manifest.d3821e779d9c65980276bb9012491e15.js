bcf6fc79d6b04232e5265d1cb0adcf67cf96e044
devkliv <devklivar@gmail.com>
2021-12-09 14:47:44 +0100

    second commit

---
+++ build/precache-manifest.d3821e779d9c65980276bb9012491e15.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbad270f1d5393821485f56ebc69db3e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "daf145189c9f15436434",
+    "url": "/static/css/2.e8233bd5.chunk.css"
+  },
+  {
+    "revision": "daf145189c9f15436434",
+    "url": "/static/js/2.e51aae90.chunk.js"
+  },
+  {
+    "revision": "899e5a3a8fbe4908a669",
+    "url": "/static/js/main.ca24d310.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "6b2572a29bd1a405610934be01aa685b",
+    "url": "/static/media/home-7-1-bg.6b2572a2.jpg"
+  },
+  {
+    "revision": "66e4c1e10d5f5dae73f6e0e67b8897f1",
+    "url": "/static/media/home-7-2-bg.66e4c1e1.jpg"
+  },
+  {
+    "revision": "76a3a8899a0222a87b898a8d5b01f29a",
+    "url": "/static/media/home-7-3-bg.76a3a889.jpg"
+  },
+  {
+    "revision": "2182330384ebbad826821fdf8e84efe5",
+    "url": "/static/media/react-bootstrap-carousel.21823303.eot"
+  },
+  {
+    "revision": "2fe6d179ea5aa1ceab26113e77e13da4",
+    "url": "/static/media/react-bootstrap-carousel.2fe6d179.svg"
+  },
+  {
+    "revision": "c746c4882df00af3b0a7a3b1616bf295",
+    "url": "/static/media/react-bootstrap-carousel.c746c488.woff"
+  },
+  {
+    "revision": "f7448c0408e7a78012a82def0fb9e2fe",
+    "url": "/static/media/react-bootstrap-carousel.f7448c04.ttf"
+  }
+]);
